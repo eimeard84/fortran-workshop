c714e3317114f9dea2c35f3ae1c54cf8fe42e124
Eimear Dunne <eimear.dunne@address.com>
2018-04-12 13:41:10 +0000

    Created Types_mod module and used NAG compiler to create unified precision

---
+++ src/Types_mod.f90
@@ -0,0 +1,20 @@
+module Types_mod
+  use, intrinsic :: iso_fortran_env
+
+  implicit none
+  ! everything is private unless otherwise stated
+  private
+  public :: SP
+  public :: DP
+  public :: SI
+  public :: DI
+
+  integer, parameter :: SP = REAL32
+  integer, parameter :: DP = REAL64
+  integer, parameter :: SI = INT32
+  integer, parameter :: DI = INT64
+  contains
+
+
+
+end module Types_mod
