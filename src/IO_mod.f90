78bf9a5049be579fade0eb5c954070498a0871cd
Eimear Dunne <eimear.dunne@address.com>
2018-04-13 14:18:32 +0000

    Fixed some bugs relating to NetCDF output

--- src/IO_mod.f90
+++ src/IO_mod.f90
@@ -1,5 +1,6 @@
 module IO_mod
   use :: types_mod, only: dp
+  use netcdf
 
   implicit none
   ! everything is private unless otherwise stated
@@ -10,31 +11,40 @@ module IO_mod
   ! Variables go here
   contains
 
-  subroutine r8mat_write(output_filename, table)
+  subroutine r8mat_write(output_filename, table, x, t)
     implicit none
 
-    integer :: m
-    integer :: n
+    integer :: m, n
 
-    integer :: j
     character (len=*), intent(in) :: output_filename
-    integer :: output_unit_id
-    character (len=30) :: string
+    integer :: ncid, tid, xid, table_id, x_dimid, t_dimid
+    integer :: ierr
     real (kind=dp), dimension(:,:), intent(in) :: table
+    real (kind=dp), dimension(:), intent(in) :: x
+    real (kind=dp), dimension(:), intent(in) :: t
 
-    m = size( table(:,:), 1 )
-    n = size( table(:,:), 2 )
+    m = size( table(:,:), 1 ) ! This is the space dimension
+    n = size( table(:,:), 2 ) ! This is the time dimension
 
-    output_unit_id = 10
-    open (unit=output_unit_id, file=output_filename, status='replace')
+    ierr = NF90_CREATE( output_filename, NF90_CLOBBER, ncid)
+      ierr = NF90_DEF_DIM( ncid, "x", m, x_dimid )
+      ierr = NF90_DEF_DIM( ncid, "t", n, t_dimid ) 
+      ierr = NF90_PUT_ATT( ncid, NF90_GLOBAL, "Purpose", "Fortran workshop") 
+      ierr = NF90_PUT_ATT( ncid, NF90_GLOBAL, "Name", "Eimear Dunne") 
+      ierr = NF90_PUT_ATT( ncid, NF90_GLOBAL, "Institution", "University of Cambridge")
+  
+      ierr = NF90_DEF_VAR( ncid, "x-range", NF90_REAL8, x_dimid, xid )
+      ierr = NF90_PUT_ATT( ncid, xid, "Units", "metres" )
+      ierr = NF90_DEF_VAR( ncid, "t-range", NF90_REAL8, t_dimid, tid )
+      ierr = NF90_PUT_ATT( ncid, tid, "Units", "seconds" )
+      ierr = NF90_DEF_VAR( ncid, "solution", NF90_REAL8, [x_dimid, t_dimid], table_id )
+      ierr = NF90_PUT_ATT( ncid, table_id, "Units", "Celsius" )
+    ierr = NF90_ENDDEF( ncid )
+      ierr = NF90_PUT_VAR( ncid, xid, x )
+      ierr = NF90_PUT_VAR( ncid, tid, t ) 
+      ierr = NF90_PUT_VAR( ncid, table_id, table )
+    ierr = NF90_CLOSE( ncid )
 
-    write (string, '(a1,i8,a1,i8,a1,i8,a1)') '(', m, 'g', 24, '.', 16, ')'
-
-    do j = 1, n
-      write (output_unit_id, string) table(1:m, j)
-    end do
-
-    close (unit=output_unit_id)
   end subroutine r8mat_write
 
   subroutine r8vec_linspace(a_first, a_last, a)
@@ -56,8 +66,6 @@ module IO_mod
   subroutine r8vec_write(output_filename, x)
 
     implicit none
-
-    integer :: m
 
     integer :: j
     character (len=*), intent(in) :: output_filename
